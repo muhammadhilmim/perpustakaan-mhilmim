2e572c017b3c0fe91ffa49680df449d45350929c
hilmi <kz363243@gmail.com>
2022-04-05 21:51:12 -0700

    perpustakaan-mhilmim

---
+++ views/beranda.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800" align="center">SELAMAT DATANG DI THE BRAIN WORLD</h1>
+    <p class="mb-4"><marquee>SEMOGA PERPUSTAKAAN INI BISA MEMBANTU</marquee></p>
+
+</div>           
